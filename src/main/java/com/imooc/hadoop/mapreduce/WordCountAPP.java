b4e26ddb31a03d3714d4e75532aef8e36d4c560c
hwbaker <hewei860311@sohu.com>
2018-03-02 16:26:41 +0800

    wordcount

---
+++ src/main/java/com/imooc/hadoop/mapreduce/WordCountAPP.java
@@ -0,0 +1,79 @@
+package com.imooc.hadoop.mapreduce;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
+/**
+ * 词频统计
+ * hdfs://localhost:19000/input/wc/hello.txt hdfs://localhost:19000/output/wc
+ */
+public class WordCountAPP {
+
+    /**
+     * Map:输入文件
+     */
+    public static class MyMapper extends Mapper<LongWritable, Text, Text, LongWritable> {
+
+        @Override
+        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            String line = value.toString();
+            String[] words = line.split("\t");
+            for (String word : words) {
+                context.write(new Text(word), new LongWritable(1));
+            }
+        }
+
+    }
+
+    /**
+     * Reduce:输出文件
+     */
+    public static class MyReduce extends Reducer<Text, LongWritable, Text, LongWritable> {
+        @Override
+        protected void reduce(Text key, Iterable<LongWritable> values, Context context) throws IOException, InterruptedException {
+            long sum = 0;
+            for (LongWritable value: values) {
+                sum += value.get();
+            }
+            context.write(key, new LongWritable(sum));
+        }
+    }
+
+    /**
+     * 封装MapReduce作业的所有信息
+     * @param args
+     */
+    public static void main(String[] args) throws Exception {
+
+        Configuration configuration = new Configuration();
+        Job job = Job.getInstance(configuration, "wordcount");
+
+        job.setJarByClass(WordCountAPP.class);
+
+        FileInputFormat.setInputPaths(job, new Path(args[0]));
+
+        job.setMapperClass(MyMapper.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputKeyClass(LongWritable.class);
+
+        job.setReducerClass(MyReduce.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(LongWritable.class);
+
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        System.exit(job.waitForCompletion(true) ? 0 : 2);
+
+
+    }
+}
