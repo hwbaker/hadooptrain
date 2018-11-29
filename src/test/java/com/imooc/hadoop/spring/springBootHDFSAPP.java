96c6662146494bb9c98d60f77d461f8393729584
hwbaker <hewei860311@sohu.com>
2018-11-29 13:27:36 +0800

    hadoopSpring

---
+++ src/test/java/com/imooc/hadoop/spring/springBootHDFSAPP.java
@@ -0,0 +1,36 @@
+package com.imooc.hadoop.spring;
+
+import org.apache.hadoop.fs.FileStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.hadoop.fs.FsShell;
+
+/**
+ * 使用 Spring Boot 访问HDFS文件系统
+ */
+
+@SpringBootApplication
+public class springBootHDFSAPP implements CommandLineRunner {
+    @Autowired
+    FsShell fsShell;
+
+    public void run(String... strings) throws Exception {
+        for (FileStatus fileStatus : fsShell.lsr("/springhdfs")) {
+            System.out.println(">" + fileStatus.getPath());
+        }
+    }
+//    public static void main(String[] args) {
+//        SpringApplication.run(springBootHDFSAPP.class, args);
+//    }
+
+
+    public static void main(String[] args) {
+        try {
+            SpringApplication.run(springBootHDFSAPP.class, args);
+        } catch (Exception e) {
+            System.out.println(e.getLocalizedMessage());
+        }
+    }
+}
